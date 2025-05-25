4fdabc8f848ec94e2a3f2031623e7e3f3c6e0325
muhumbulo <makhadolucky@gmail.com>
2025-05-26 00:11:07 +0200

    Update order with product

--- src/main/java/com/example/store/dto/OrderDTO.java
+++ src/main/java/com/example/store/dto/OrderDTO.java
@@ -3,10 +3,12 @@ package com.example.store.dto;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Data
 public class OrderDTO implements Serializable {
     private Long id;
     private String description;
     private OrderCustomerDTO customer;
+    private List<ProductDTO> products;
 }
--- src/main/java/com/example/store/entity/Order.java
+++ src/main/java/com/example/store/entity/Order.java
@@ -12,7 +12,8 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String description;
+    @ManyToMany(fetch = FetchType.LAZY)
+    private Product product;
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Customer customer;
