30be00afeaf663e55e852a55ea2431a4832f1712
muhumbulo <makhadolucky@gmail.com>
2025-05-25 11:50:08 +0200

    Add exception for duplicate name

--- src/main/java/com/example/store/repository/CustomerRepository.java
+++ src/main/java/com/example/store/repository/CustomerRepository.java
@@ -4,4 +4,8 @@ import com.example.store.entity.Customer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository extends JpaRepository<Customer, Long> {}
+import java.util.Optional;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByName(String name);
+}
--- src/main/java/com/example/store/service/CustomerService.java
+++ src/main/java/com/example/store/service/CustomerService.java
@@ -1,12 +1,15 @@
 package com.example.store.service;
 
 import com.example.store.dto.CustomerDTO;
+import com.example.store.entity.Customer;
+import com.example.store.exception.exceptions.AlreadyExistException;
 import com.example.store.mapper.CustomerMapper;
 import com.example.store.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +23,12 @@ public class CustomerService {
     }
 
     public CustomerDTO createCustomer(CustomerDTO customerDTO){
-        return customerMapper.customerToCustomerDTO(customerRepository.save(customerMapper.customerDtoToCustomer(customerDTO)));
+        Optional<Customer> customerOptional = customerRepository.findByName(customerDTO.getName());
+        if ( customerOptional.isEmpty() ){
+            return customerMapper.customerToCustomerDTO(customerRepository.save(customerMapper.customerDtoToCustomer(customerDTO)));
+        } else {
+            String message = String.format("Customer with name %s already exist", customerDTO.getName());
+            throw new AlreadyExistException(message, message);
+        }
     }
 }
