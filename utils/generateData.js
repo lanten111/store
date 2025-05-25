4f6dd74a96dd18f430a2eaa29c86aeb56017be7b
muhumbulo <makhadolucky@gmail.com>
2025-05-25 11:25:26 +0200

    Update generate data script to not insert id

--- utils/generateData.js
+++ utils/generateData.js
@@ -5,11 +5,11 @@ const M = 10_000; // Number of orders
 
 // Generate customers
 for (let i = 1; i <= N; i++) {
-    console.log(`INSERT INTO customer (id, name) VALUES (${i}, '${faker.name.fullName()}');`);
+    console.log(`INSERT INTO customer (name) VALUES ('${faker.name.fullName()}');`);
 }
 
 // Generate orders
 for (let i = 1; i <= M; i++) {
     const customerId = Math.ceil(Math.random() * N);
-    console.log(`INSERT INTO "order" (id, description, customer_id) VALUES (${i}, '${faker.commerce.productName()}', ${customerId});`);
+    console.log(`INSERT INTO "order" (description, customer_id) VALUES ('${faker.commerce.productName()}', ${customerId});`);
 }
