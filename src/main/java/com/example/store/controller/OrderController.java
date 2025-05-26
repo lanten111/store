e4740653230128207d2277823dc9e4b3912f6c8f
muhumbulo <makhadolucky@gmail.com>
2025-05-26 18:54:38 +0200

    Add validation for order

--- src/main/java/com/example/store/controller/OrderController.java
+++ src/main/java/com/example/store/controller/OrderController.java
@@ -6,9 +6,12 @@ import com.example.store.mapper.OrderMapper;
 import com.example.store.repository.OrderRepository;
 
 import com.example.store.service.OrderService;
+import com.example.store.validation.OnCreate;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,12 +30,12 @@ public class OrderController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public OrderDTO createOrder(@RequestBody OrderDTO orderDTO) {
+    public OrderDTO createOrder(@Validated(OnCreate.class) @RequestBody OrderDTO orderDTO) {
         return orderService.createOrder(orderDTO);
     }
 
     @GetMapping("/{orderId}")
-    public OrderDTO getOrder(@PathVariable Long orderId){
+    public OrderDTO getOrder(@PathVariable @NotBlank Long orderId){
         return orderService.getOrderById(orderId);
     }
 }
--- src/main/java/com/example/store/dto/OrderDTO.java
+++ src/main/java/com/example/store/dto/OrderDTO.java
@@ -1,5 +1,8 @@
 package com.example.store.dto;
 
+import com.example.store.validation.OnCreate;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -8,6 +11,8 @@ import java.util.List;
 @Data
 public class OrderDTO implements Serializable {
     private Long orderId;
+    @NotNull(message = "{order.create.empty.customer}", groups = OnCreate.class)
     private OrderCustomerDTO customer;
+    @NotEmpty(message = "{order.create.empty.product}", groups = OnCreate.class)
     private List<OrderProductDTO> products;
 }
