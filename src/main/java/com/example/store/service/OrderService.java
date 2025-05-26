f58ee8cbedb46d0b4aca89338b8814d2d9f1703b
muhumbulo <makhadolucky@gmail.com>
2025-05-26 15:52:33 +0200

    Update create order service

--- src/main/java/com/example/store/service/OrderService.java
+++ src/main/java/com/example/store/service/OrderService.java
@@ -1,7 +1,9 @@
 package com.example.store.service;
 
 import com.example.store.dto.OrderDTO;
+import com.example.store.dto.OrderProductDTO;
 import com.example.store.entity.Order;
+import com.example.store.entity.Product;
 import com.example.store.exception.exceptions.NotFoundException;
 import com.example.store.mapper.OrderMapper;
 import com.example.store.repository.OrderRepository;
@@ -11,6 +13,7 @@ import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,6 +26,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final OrderMapper orderMapper;
+    private final ProductService productService;
 
     @Cacheable(value = ORDER_DTO_CACHE_NAME, key = "#orderId")
     public OrderDTO getOrderById(Long orderId){
@@ -35,10 +39,17 @@ public class OrderService {
         }
     }
 
-    @CachePut(cacheNames = ORDER_DTO_CACHE_NAME, key = "#result.id")
+    @CachePut(cacheNames = ORDER_DTO_CACHE_NAME, key = "#result.orderId")
     @CacheEvict(cacheNames = ORDER_LIST_DTO_CACHE_NAME, allEntries = true)
     public OrderDTO createOrder(OrderDTO orderDTO){
-        return orderMapper.orderToOrderDTO(orderRepository.save(orderMapper.orderDtoToOrder(orderDTO)));
+        List<Product> products = new ArrayList<>();
+        for (OrderProductDTO dto: orderDTO.getProducts()){
+            Product product = productService.getProductEntity(dto.getProductId());
+            products.add(product);
+        }
+        Order order = orderMapper.orderDtoToOrder(orderDTO);
+        order.setProducts(products);
+        return orderMapper.orderToOrderDTO(orderRepository.save(order));
     }
 
 
