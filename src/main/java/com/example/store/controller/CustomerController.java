179098a5627f89e9b62043514b8364ffb654234c
muhumbulo <makhadolucky@gmail.com>
2025-05-26 18:43:03 +0200

    Add validation on customer

--- src/main/java/com/example/store/controller/CustomerController.java
+++ src/main/java/com/example/store/controller/CustomerController.java
@@ -6,9 +6,13 @@ import com.example.store.mapper.CustomerMapper;
 import com.example.store.repository.CustomerRepository;
 
 import com.example.store.service.CustomerService;
+import com.example.store.validation.OnCreate;
+import com.example.store.validation.OnSearch;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,12 +31,12 @@ public class CustomerController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public CustomerDTO createCustomer(@RequestBody CustomerDTO customerDTO) {
+    public CustomerDTO createCustomer(@Validated(OnCreate.class)  @RequestBody CustomerDTO customerDTO) {
         return customerService.createCustomer(customerDTO);
     }
 
     @GetMapping("/search/{query}")
-    public List<CustomerDTO> searchCustomer(@PathVariable String query){
+    public List<CustomerDTO> searchCustomer( @PathVariable @NotBlank  String query ){
         return customerService.customerSearch(query);
     }
 }
--- src/main/java/com/example/store/dto/CustomerDTO.java
+++ src/main/java/com/example/store/dto/CustomerDTO.java
@@ -1,5 +1,8 @@
 package com.example.store.dto;
 
+import com.example.store.validation.OnCreate;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -8,6 +11,8 @@ import java.util.List;
 @Data
 public class CustomerDTO implements Serializable {
     private Long customerId;
+    @NotBlank(message = "{customer.create.empty.name}", groups = OnCreate.class)
+    @Size(min = 8, max = 20 , message = "{product.create.min.max.name}")
     private String name;
     private List<CustomerOrderDTO> orders;
 }
