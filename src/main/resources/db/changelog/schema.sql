eee7b21573141ca98e2fa3497c5cb730c9d95596
muhumbulo <makhadolucky@gmail.com>
2025-05-26 15:51:44 +0200

    Update schema

--- src/main/resources/db/changelog/schema.sql
+++ src/main/resources/db/changelog/schema.sql
@@ -1,13 +1,29 @@
 -- Create customer table
 CREATE TABLE customer (
-                          id BIGSERIAL PRIMARY KEY,
+                          customer_id BIGSERIAL PRIMARY KEY,
                           name VARCHAR(255) NOT NULL
 );
 
 -- Create order table
 CREATE TABLE "order" (
-                         id BIGSERIAL PRIMARY KEY,
-                         description VARCHAR(255) NOT NULL,
+                         order_id BIGSERIAL PRIMARY KEY,
                          customer_id BIGINT NOT NULL,
-                         CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES customer (id)
+                         CONSTRAINT fk_customer FOREIGN KEY (customer_id) REFERENCES customer (customer_id)
+);
+
+-- Create product table
+CREATE TABLE "product" (
+                         product_id BIGSERIAL PRIMARY KEY,
+                         description VARCHAR(255) NOT NULL,
+                         name VARCHAR(255) NOT NULL
+);
+
+-- Create order product link table
+CREATE TABLE "order_product" (
+                          product_id BIGSERIAL NOT NULL,
+                          order_id BIGSERIAL NOT NULL,
+                          PRIMARY KEY (order_id, product_id),
+                          FOREIGN KEY (order_id) REFERENCES "order"(order_id),
+                          FOREIGN KEY (product_id) REFERENCES product(product_id)
+
 );
