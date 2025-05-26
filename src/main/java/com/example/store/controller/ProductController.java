d8f1fb0e7fa3803fd1e8873a52075e8bfb07426b
muhumbulo <makhadolucky@gmail.com>
2025-05-26 15:53:14 +0200

    Fix product controller annotation

--- src/main/java/com/example/store/controller/ProductController.java
+++ src/main/java/com/example/store/controller/ProductController.java
@@ -3,12 +3,12 @@ package com.example.store.controller;
 import com.example.store.dto.ProductDTO;
 import com.example.store.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/product")
 @RequiredArgsConstructor
 public class ProductController {
@@ -16,6 +16,7 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public ProductDTO createProduct(@RequestBody ProductDTO productDTO){
         return productService.createProduct(productDTO);
     }
