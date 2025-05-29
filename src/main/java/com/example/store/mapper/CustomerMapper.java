620c5195b5af18272cd7bf68bfefb8c929b2fbd7
muhumbulo <makhadolucky@gmail.com>
2025-05-29 19:22:05 +0200

    Add password ignore

--- src/main/java/com/example/store/mapper/CustomerMapper.java
+++ src/main/java/com/example/store/mapper/CustomerMapper.java
@@ -4,14 +4,19 @@ import com.example.store.dto.CustomerDTO;
 import com.example.store.entity.Customer;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CustomerMapper {
+
+    @Mapping(target = "password", ignore = true)
     CustomerDTO customerToCustomerDTO(Customer customer);
 
+    @Mapping(target = "password", ignore = true)
     List<CustomerDTO> customersToCustomerDTOs(List<Customer> customer);
 
+    @Mapping(target = "password", ignore = true)
     Customer customerDtoToCustomer(CustomerDTO customerDTO);
 }
