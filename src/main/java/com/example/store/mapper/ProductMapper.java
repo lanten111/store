4eea252eb5da6971c8b5cdc427433d6f036ed9c9
muhumbulo <makhadolucky@gmail.com>
2025-05-29 19:24:04 +0200

    Update mapper

--- src/main/java/com/example/store/mapper/ProductMapper.java
+++ src/main/java/com/example/store/mapper/ProductMapper.java
@@ -1,8 +1,6 @@
 package com.example.store.mapper;
 
 import com.example.store.dto.ProductDTO;
-import com.example.store.dto.ProductOrderDTO;
-import com.example.store.entity.Order;
 import com.example.store.entity.Product;
 
 import org.mapstruct.Mapper;
@@ -14,11 +12,7 @@ public interface ProductMapper {
 
     ProductDTO productToProductDTO(Product product);
 
-    List<ProductDTO> productsTOProductDTOs(List<Product> products);
+    List<ProductDTO> productsToProductDTOs(List<Product> products);
 
-    ProductOrderDTO productToProductOrder(Order order);
-
-    List<ProductOrderDTO> productsToProductOrderDTOs(List<Order> orders);
-
-    Product productDTOToProduct(ProductDTO productDTO);
+    Product productDtoToProduct(ProductDTO productDTO);
 }
